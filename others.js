a5b9807ea7b665fa934538a5a6dc868f0078f5d3
graceoflives <tienngocbui2612@gmail.com>
2016-12-17 12:36:34 +0700

    Update others.js

--- others.js
+++ others.js
@@ -1,5 +1,10 @@
-function test_123() {
-    var a=999;
-    var b=120;
-    alert(a+b);
+function generateSaveGame() {
+    var result=rawData;
+    for (var k in ancient)
+        if (ancient[k].Level.equals(0)==false) {
+            result.ancients.ancients[k].level=ancient[k].Level.toExponential();
+            result.ancients.ancients[k].spentHeroSouls=Decimal(result.ancients.ancients[k].spentHeroSouls).plus(ancient[k].CostToOptimal).toExponential();
+            result.heroSouls=Decimal(result.heroSouls).minus(ancient[k].CostToOptimal).toExponential();
+        }
+	return JSON.stringify(result);
 }
