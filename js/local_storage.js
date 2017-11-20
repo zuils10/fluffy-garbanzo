cdec3413ca1e65f4627348f471b619ca2337bba1
ngocbt <tienngocbui2612@gmail.com>
2017-11-20 14:48:33 +0700

    remember more stuff

--- js/local_storage.js
+++ js/local_storage.js
@@ -6,16 +6,43 @@ var ls = {
 	applySettings: function() {
 		var object_ls = window.localStorage;
 		//read theme and apply
-		var _saved_theme = object_ls.getItem('theme');
-		if (_saved_theme == 1) {
-			$('#useDarkTheme').prop('checked', _saved_theme).trigger('change');
+		var _saved = object_ls.getItem('theme');
+		if (_saved == 1) {
+			$('#useDarkTheme').prop('checked', _saved).trigger('change');
+		}
+
+		//read flagCalculateOtherAncients and apply
+		_saved = object_ls.getItem('flagCalculateOtherAncients');
+		if (_saved == 1) {
+			$('#flagCalculateOtherAncients').prop('checked', _saved).trigger('change');
+		}
+
+		//read otherAncientsLower and apply
+		var _saved = object_ls.getItem('otherAncientsLower');
+		if (_saved != null) {
+			$('#otherAncientsLower').val(_saved).trigger('change');
+		}
+
+		//read otherAncientsLowerBase and apply
+		var _saved = object_ls.getItem('otherAncientsLowerBase');
+		if (_saved != null) {
+			$('#otherAncientsLowerBase').val(_saved).trigger('change');
 		}
 	},
 	saveSettings: function() {
 		var object_ls = window.localStorage;
 		$('#useDarkTheme').on('change', function() {
 			object_ls.setItem('theme', $('#useDarkTheme').prop('checked') ? 1 : 0);
-		})
+		});
+		$('#flagCalculateOtherAncients').on('change', function() {
+			object_ls.setItem('flagCalculateOtherAncients', $('#flagCalculateOtherAncients').val() ? 1 : 0);
+		});
+		$('#otherAncientsLower').on('change', function() {
+			object_ls.setItem('otherAncientsLower', $('#otherAncientsLower').val());
+		});
+		$('#otherAncientsLowerBase').on('change', function() {
+			object_ls.setItem('otherAncientsLowerBase', $('#otherAncientsLowerBase').val());
+		});
 	}
 };
 
